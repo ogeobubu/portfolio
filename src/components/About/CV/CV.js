ad4eb2b4077fd1d9d1264149a8276e5e9f4f089f
Oge Obubu <ogeobubu@gmail.com>
2022-01-11 06:58:47 +0100

    updating portfolio

--- src/components/About/CV/CV.js
+++ src/components/About/CV/CV.js
@@ -1,20 +1,19 @@
-import React from 'react'
-import './cv.css'
-import Button from '../../Buttons/Buttons'
+import React from "react";
+import "./cv.css";
+import Button from "../../Buttons/Buttons";
 import { Link } from "react-router-dom";
-import myCV from '../../img/OGE OBUBU RESUME.pdf'
-
+import myCV from "../../img/ogeobubu.pdf";
 
 const CV = () => {
-    return (
-        <div className="cv">
-            <Link className="link" to={myCV} target="_blank" download>
-                <Button buttonStyle="btn--secondary" buttonSize="btn-md">
-                    Download Resume
-                </Button>
-            </Link>
-        </div>
-    )
-}
+  return (
+    <div className="cv">
+      <Link className="link" to={myCV} target="_blank" download>
+        <Button buttonStyle="btn--secondary" buttonSize="btn-md">
+          Download Resume
+        </Button>
+      </Link>
+    </div>
+  );
+};
 
-export default CV
+export default CV;
